899c120ba1537339b2ffac1dc76d4a1e9bb4d95f
alejandrogualdron <alejandrogualdron@users.noreply.github.com>
2021-01-26 18:00:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/serviciosrestapi/PostService.java
@@ -0,0 +1,11 @@
+package com.example.serviciosrestapi;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostService {
+    String API_ROUTE="/posts";
+    @GET(API_ROUTE)
+    Call<List<PostClass>>getPost();
+}
